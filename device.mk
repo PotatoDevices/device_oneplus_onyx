424a17140bc46232c2910bb6213a5e890a1f8baf
Kevin F. Haggerty <haggertk@lineageos.org>
2020-11-28 07:17:09 -0700

    onyx: Use libprotobuf-cpp-lite-v29.so for libwvdrmengine.so
    * libwvdrmengine.so needs _ZN6google8protobuf8internal13empty_string_E
      which is dropped in the API 30 library, but can be found in the
      API 29 prebuilt
    
    Change-Id: I39e3251d53b14f3325282b12e6a6bf741ebcf63a

--- device.mk
+++ device.mk
@@ -307,6 +307,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf_shim
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
